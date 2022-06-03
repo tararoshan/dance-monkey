e5f50c2c8da81a7686f72c2a02e2fa3c82a9e934
tararoshan <troshan@utexas.edu>
2022-06-02 19:22:01 -0700

    Changing speed and mirroring now works on YouTube pages in firefox

--- mirror.js
+++ mirror.js
@@ -2,7 +2,16 @@ alert(window.location);
 var vid = document.querySelector('video');
 
 if (vid != null) {
-    alert('found a video to mirror');
+    // found a video to mirror
+    vid.style.transform = 'scaleX(-1)';
 } else {
-    alert('couldn\'t find a video');
+    // couldn't find a video, try to find iframes
+    var myFrame = document.querySelector('iframe');
+    myFrame.style.transform = 'scaleX(-1)';
 }
+
+/* TODO
+ * -add reset & refresh instructions
+ * -let ppl know about iframe-related issues
+ *  
+ * */
--- popup/options.js
+++ popup/options.js
@@ -1,8 +1,14 @@
 /* initialise variables */
 var mirrorBox = document.getElementById('mirror');
+var speedSlider = document.getElementById('speed-range');
+var speedNum = document.getElementById('speed-num');
+var loopBox = document.getElementById('loop');
 
 /*  add event listeners to inputs */
-mirrorBox.addEventListener('click', mirrorFunc);
+mirrorBox.addEventListener('change', mirrorFunc);
+speedSlider.addEventListener('change', syncFromSlider);
+speedNum.addEventListener('change', syncFromNum);
+loopBox.addEventListener('change', loop);
 
 /* generic error handler */
 function onError(error) {
@@ -13,23 +19,84 @@ function onError(error) {
 initialize();
 
 function initialize() {
-    if (localStorage.length > 0 ) {
-        mirrorBox.value = localStorage.getItem('mirrorBox');
-        mirrorBox.checked = mirrorBox.value === 'on' ? true : false;
-    }
+    // mirror
+    mirrorBox.value = localStorage.getItem('mirrorBox');
+    mirrorBox.checked = mirrorBox.value === 'on' ? true : false;
+    // speed
+    speedSlider.value = localStorage.getItem('speed');
+    speedNum.value = localStorage.getItem('speed');
 }
 
+/* mirror button function */
 function mirrorFunc() {
     if (mirrorBox.value === 'on') {
         // if the video is mirrored, unmirror it
         mirrorBox.value = 'off';
         localStorage.setItem('mirrorBox', 'off');
         alert('mirror now off');
+        // run script
     } else if (mirrorBox.value === 'off') {
         // otherwise, mirror the video
         mirrorBox.value = 'on';
         localStorage.setItem('mirrorBox', 'on');
         alert('mirror now on');
-        // browser.tabs.executeScript({file: `/mirror.js`, allFrames: true});
+        browser.tabs.executeScript({file: `/mirror.js`, allFrames: true});
     }
 }
+
+/* syncing the speed slider and number */
+function syncFromSlider() {
+    let newSpeed = speedSlider.value;
+    speedNum.value = newSpeed;
+    changeSpeed(newSpeed);
+}
+
+function syncFromNum() {
+    let newSpeed = speedNum.value;
+    speedSlider.value = newSpeed;
+    changeSpeed(newSpeed);
+}
+
+// executes script assuming no iframes
+function changeSpeed(newSpeed) {
+    localStorage.setItem('speed', newSpeed);
+    
+    let speedCode = `var vid = document.querySelector('video');\n vid.playbackRate = ${newSpeed};`
+    browser.tabs.executeScript({
+        code: speedCode
+    })
+}
+
+/* loop */
+// function loop() {
+//     if (loopBox.value === 'on') {
+//         // if the video is looped, unloop it
+//         loopBox.value = 'off';
+//         localStorage.setItem('loopBox', 'off');
+//         alert('loop now off');
+//     } else if (loopBox.value === 'off') {
+//         // otherwise, loop the video
+//         loopBox.value = 'on';
+//         localStorage.setItem('loopBox', 'on');
+//         alert('loop now on');
+//     }
+// }
+// // if time goes over the ending min/sec, set it to starting time/sec
+// // if below starting time/sec, set it to starting time/sec
+// // set max time/sec values for input to corresponding max for video
+// let durationCode = `var vid = document.querySelector('video');\n vid.duration;`
+
+// browser.tabs.executeScript({
+//     code: durationCode
+// }, setMaxTime)
+
+// function setMaxTime(resultsArray) {
+//     alert(resultsArray[0]);
+// }
+
+// // delay
+// function delay(timeout) {
+//     // remove the hourglass button
+//     // delayButton.style = 
+//     let timeoutCode = `var vid = document.querySelector('video); vid.pause(); setTimeout(() => {vid.play()}, ${timeout} * 1000);`
+// }
