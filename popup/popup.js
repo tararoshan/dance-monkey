d1431c32e987af072b77fe321ac1b8f7766a7580
Tara Roshan <tara.roshan12@gmail.com>
2024-01-30 21:35:56 -0600

    WIP video looping, cleaned up code
    Since the execution context is cleared, and *bound parameters are lost*,
    I'll have to use messaging to pass in the loop start/stop times.

--- popup/popup.js
+++ popup/popup.js
@@ -5,6 +5,7 @@
  */
 const MAX_SPEED = 10;
 const MIN_SPEED = 0.1;
+const SECONDS_PER_MIN = 60;
 
 // Specifically for debugging, so I don't have to comment stuff out
 const DEGUG = true;
@@ -30,10 +31,10 @@ var speedSlider = document.getElementById("speed-range");
 
 var loopCheckbox = document.getElementById("loop-checkbox");
 loopCheckbox.addEventListener("change", onLoopChange);
-var loopMinutesStart = document.getElementById("loop-minutes-start-num");
-var loopSecondsStart = document.getElementById("loop-seconds-start-num");
-var loopMinutesEnd = document.getElementById("loop-minutes-end-num");
-var loopSecondsEnd = document.getElementById("loop-seconds-end-num");
+let minStartElem = document.getElementById("loop-minutes-start-num");
+let secStartElem = document.getElementById("loop-seconds-start-num");
+let minEndElem = document.getElementById("loop-minutes-end-num");
+let secEndElem = document.getElementById("loop-seconds-end-num");
 
 /**
  * MIRROR VIDEO LISTENER
@@ -67,39 +68,30 @@ async function onLoopChange() {
 
 	if (loopCheckbox.checked) {
 		document.getElementById("loop-div").style.opacity = 1;
-		const SECONDS_PER_MIN = 60;
-		debugMessage('number inputs:', 
-			document.getElementById("loop-minutes-start-num"), 
-			document.getElementById("loop-seconds-start-num"),
-			document.getElementById("loop-minutes-end-num"), 
-			document.getElementById("loop-seconds-end-num"))
 
-		// TODO figure out the default input (or set it to something?)
-		debugMessage(`is input null? ${document.getElementById("loop-minutes-start-num") == null}`)
-		// Set up the looping (browser script)
+		// Wait until all of the boxes have some input
+		function waitingWhileLoop() {
+			if (minStartElem.value == null || secStartElem.value == null 
+					|| minEndElem.value == null || secEndElem.value == null) {
+				
+				console.log("some element is null...")
+				setTimeout(waitingWhileLoop, 5_000);
+			}
+		}
+
+		// Check that the input is valid
+		let loopStart = minStartElem * SECONDS_PER_MIN + secStartElem;
+		let loopStop = minEndElem * SECONDS_PER_MIN + secEndElem;
+
+		// Set up the video section looping (browser script)
 		browser.scripting.executeScript({
-			func: loopVideoBrowserScript,
+			func: loopVideoBrowserScript(loopStart, loopStop),
 			target: {
 				tabId: await getActiveTabId(),
 				allFrames: true
 			}
 		})
 		
-		let loopStart = document.getElementById("loop-minutes-start-num") 
-						* SECONDS_PER_MIN
-						+ document.getElementById("loop-seconds-start-num");
-		// set to length of video or the loop stop, whichever is less
-		let loopStop = document.getElementById("loop-minutes-end-num") 
-						* SECONDS_PER_MIN 
-						+ document.getElementById("loop-seconds-end-num");
-		// change currentTime property
-		function loopVideoListener() {
-			debugMessage("the this object: ", this)
-			if (this.currentTime >= loopStop || this.currentTime < loopStart) {
-				vid.currentTime = loopStart;
-			}
-			debugMessage("updated time")
-		}
 		// can do messaging if this doesn't work
 	} else{
 		// if not checked, make the minute/seconds numbers hidden
@@ -137,11 +129,24 @@ function unmirrorVideoBrowserScript() {
 	vid.style.transform = "";
 }
 
-function loopVideoBrowserScript() {
+function loopVideoListener(loopStart, loopStop, vidDuration) {
+	debugMessage("the this object: ", this)
+	loopStop = loopStop < vidDuration ? loopStop : vidDuration;
+
+	if (this.currentTime >= loopStop || this.currentTime < loopStart) {
+		this.currentTime = loopStart;
+		debugMessage(`updated time to ${this.currentTime}`)
+	} else {
+		debugMessage("no time update!")
+	}
+}
+
+function loopVideoBrowserScript(loopStart, loopStop) {
 	var vid = document.querySelector("video");
 	if (!vid) {
 		debugMessage("couldn't find video in loopVideoListener function");
 	} else {
-		vid.addEventListener("timeupdate", loopVideoListener)
+		vid.addEventListener("timeupdate", 
+			loopVideoListener(loopStart, loopStop, vid.duration))
 	}
 }
