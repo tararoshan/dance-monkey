31109ae781c349b68c909e36cda537fdd1567064
Tara Roshan <tara.roshan12@gmail.com>
2024-02-10 18:58:28 -0600

    bug: parse loop stop/start as Number
    I currently have an issue where the video isn't found in the
    loopVideoContentScript function. I'm not sure why. The function is being
    called the same way that the mirrorContentScript func is called.

--- popup/popup.js
+++ popup/popup.js
@@ -16,14 +16,13 @@ function debugMessage(message) {
 		console.log(message);
 	}
 }
-
 debugMessage("RUNNING EXTENSION CODE!!!!!!!!!");
 
 /**
- * ADD LISTENERS TO EXTENSION ELEMENTS
+ * ADD EVENT LISTENERS TO EXTENSION ELEMENTS
  */
 var mirrorCheckbox = document.getElementById("mirror-checkbox");
-mirrorCheckbox.addEventListener("click", mirrorListener);
+mirrorCheckbox.addEventListener("click", mirrorHandler);
 
 // var speedSlider = document.getElementById("speed-range");
 // speedSlider.addEventListener("change", speedListener(0));
@@ -31,32 +30,32 @@ mirrorCheckbox.addEventListener("click", mirrorListener);
 // speedNum.addEventListener("change", speedListener(1));
 
 var loopIcon = document.getElementById("loop-icon");
-loopIcon.addEventListener("change", loopListner);
+loopIcon.addEventListener("click", loopHandler);
 var loopStartMinsec = document.getElementById("loop-start-minsec");
 var loopStopMinsec = document.getElementById("loop-stop-minsec");
+var loopMessage = document.getElementById("loop-message-box");
 
 var pasteLoopStart = document.getElementById("paste-loop-start");
-pasteLoopStart.addEventListener("click", pasteLoopTimeListener);
+pasteLoopStart.addEventListener("click", pasteLoopTimeHandler);
 var pasteLoopStop = document.getElementById("paste-loop-stop");
-pasteLoopStop.addEventListener("click", pasteLoopTimeListener);
+pasteLoopStop.addEventListener("click", pasteLoopTimeHandler);
 
 /**
- * MIRROR VIDEO LISTENER
+ * MIRROR VIDEO HANDLER
  */
-async function mirrorListener() {
+async function mirrorHandler() {
 	debugMessage("the mirror checkbox was just changed");
-	var funcToExecute = null;
-
+	
+	var isMirrorRequest = false;
 	// Mirror or unmirror the video depending on checkbox status
 	if (mirrorCheckbox.checked) {
-		funcToExecute = mirrorVideoContentScript;
+		isMirrorRequest = true;
 		debugMessage("going to run the mirror script");
-	} else {
-		funcToExecute = unmirrorVideoContentScript;
 	}
 
 	browser.scripting.executeScript({
-		func: funcToExecute,
+		args: [isMirrorRequest],
+		func: mirrorContentScript,
 		target: {
 			tabId: await getActiveTabId(),
 			allFrames: true,
@@ -65,95 +64,76 @@ async function mirrorListener() {
 }
 
 /**
- * PLAYBACK SPEED LISTENER
+ * PLAYBACK SPEED HANDLER
  */
-function speedListener(inputNum) {
+async function speedHandler(inputNum) {
 	if (inputNum == SLIDER_INPUT) {
 		// todo
 	}
+	var newSpeed = 0.5;
+	// let speedCode = `var vid = document.querySelector('video');\n vid.playbackRate = ${newSpeed};`;
+	// browser.tabs.executeScript({
+	// 	code: speedCode,
+	// });
+
+	browser.scripting.executeScript({
+		args: [newSpeed],
+		func: (newSpeed) => {
+			var vid = document.querySelector("video");
+			vid.playbackRate = newSpeed;
+		},
+		target: {
+			tabId: await getActiveTabId(),
+			allFrames: true,
+		},
+	});
 }
 
 /**
- * LOOP VIDEO LISTENER
+ * LOOP VIDEO HANDLER
  */
-async function loopListner() {
-	debugMessage("loop checkbox was just changed");
+async function loopHandler() {
+	debugMessage("loop handler triggered");
 
-	if (loopCheckbox.checked) {
-		// document.getElementById("loop-div").style.display = ;
+	// Get the loop start & stop time
+	let loopStartTime = parseInputTime(loopStartMinsec.value);
+	let loopStopTime = parseInputTime(loopStopMinsec.value);
 
-		// Wait until all of the boxes have some input
-		// if (
-		// 	minStartElem.value == null ||
-		// 	secStartElem.value == null ||
-		// 	minEndElem.value == null ||
-		// 	secEndElem.value == null
-		// ) {
-		// 	debugMessage("some element is null...");
-		// 	setTimeout(waitingWhileLoop, 5_000);
-		// } else {
-		// Check that the input is valid
-		// var loopStart = minStartElem * SECONDS_PER_MIN + secStartElem;
-		// var loopStop = minEndElem * SECONDS_PER_MIN + secEndElem;
-		// var playbackSpeed = 5;
-
-		var newSpeed = 0.5;
-		// let speedCode = `var vid = document.querySelector('video');\n vid.playbackRate = ${newSpeed};`;
-		// browser.tabs.executeScript({
-		// 	code: speedCode,
-		// });
-
-		browser.scripting.executeScript({
-			args: [newSpeed],
-			func: (newSpeed) => {
-				var vid = document.querySelector("video");
-				vid.playbackRate = newSpeed;
-			},
-			target: {
-				tabId: await getActiveTabId(),
-				allFrames: true,
-			},
-		});
-
-		// Set up the video section looping (browser script)
-		// browser.scripting.executeScript({
-		// 	func: () => {
-		// 		console.log("test1")
-		// 		var vid = document.querySelector("video");
-		// 		vid.playbackRate = playbackSpeed;
-		// 		console.log("test2")
-		// 		if (!vid) {
-		// 			debugMessage(
-		// 				"couldn't find video in loopVideoListener function"
-		// 			);
-		// 		} else {
-		// 			console.log("found video, adding timeupdate listener")
-		// 			let vidDuration = vid.duration;
-		// 			loopStop = loopStop < vidDuration ? loopStop : vidDuration;
-		// 			console.log(`start time: ${loopStart}, stop time: ${loopStop}`)
-
-		// 			vid.addEventListener(
-		// 				"timeupdate",
-		// 				loopVideoListener(vid.duration)
-		// 			);
-		// 		}
-		// 	},
-		// 	target: {
-		// 		tabId: await getActiveTabId(),
-		// 		allFrames: true,
-		// 	},
-		// });
-
-		// can do messaging if this doesn't work
+	// Check that loop stop is after loop start, show the message otherwise
+	if (loopStartTime >= loopStopTime) {
+		loopMessage.style.display = "inherit";
+		return;
 	} else {
-		// if not checked, make the minute/seconds numbers hidden
-		document.getElementById("loop-div").style.opacity = 0;
-		// Remove the looping
-		// vid.removeEventListener("timeupdate", loopVideoListener);
+		loopMessage.style.display = "none";
 	}
+
+	debugMessage(`START: ${loopStartTime}, STOP: ${loopStopTime}`)
+	// Add event listener for the video time to loop
+	browser.scripting.executeScript({
+		args: [loopStartTime, loopStopTime],
+		func: loopVideoContentScript,
+		target: {
+			tabId: await getActiveTabId(),
+			allFrames: true,
+		},
+	});
 }
 
-async function pasteLoopTimeListener(event) {
+function parseInputTime(inputTimeString) {
+	let timeArray = inputTimeString.split(":");
+	let timeInSeconds =
+		timeArray.length > 1
+			? Number(timeArray[0]) * 60 + Number(timeArray[1])
+			: Number(timeArray[0]);
+
+	return timeInSeconds;
+}
+
+
+/**
+ * CLIPBOARD PASTE CURRENT TIME HANDLER
+ */
+async function pasteLoopTimeHandler(event) {
 	// Get current time of video
 	let injectionResultFrames = await browser.scripting.executeScript({
 		func: () => {
@@ -175,6 +155,7 @@ async function pasteLoopTimeListener(event) {
 		);
 		return;
 	}
+
 	// Change into min:sec format, assuming the video is less than an hour
 	let sec = currentTime % 60;
 	let min = (currentTime - sec) / 60;
@@ -189,7 +170,10 @@ async function pasteLoopTimeListener(event) {
 	}
 }
 
-function delayListener() {
+/**
+ * DELAY HANDLER
+ */
+function delayHandler() {
 	// todo
 }
 
@@ -205,42 +189,36 @@ async function getActiveTabId() {
 	return undefined;
 }
 
-function mirrorVideoContentScript() {
+function mirrorContentScript(isMirrorRequest) {
 	var vid = document.querySelector("video");
 	if (!vid) {
-		debugMessage("couldn't find video in mirrorVideo function");
-	}
-	vid.style.transform = "scaleX(-1)";
-}
-
-function unmirrorVideoContentScript() {
-	var vid = document.querySelector("video");
-	if (!vid) {
-		debugMessage("couldn't find video in unmirrorVideo function");
-	}
-	vid.style.transform = "";
-}
-
-function loopVideoContentScript() {
-	var vid = document.querySelector("video");
-	if (!vid) {
-		debugMessage("couldn't find video in loopVideoListener function");
+		console.log("[DM] Couldn't find video in mirrorContentScript function");
+	} else if (isMirrorRequest) {
+		vid.style.transform = "scaleX(-1)";
 	} else {
-		vid.addEventListener("timeupdate", loopVideoListener(vid.duration));
+		vid.style.transform = "";
 	}
 }
 
-// function loopVideoListener(vidDuration) {
-// 	debugMessage("the this object: ", this);
-// 	loopStop = loopStop < vidDuration ? loopStop : vidDuration;
-// 	debugMessage(`loop start ${loopStart}, loop stop: ${loopStop}, duration: ${vidDuration}`)
+function loopVideoContentScript(loopStartTime, loopStopTime) {
+	var vid = document.querySelector("video");
+	if (!vid) {
+		console.log("[DM] Couldn't find video in loopVideoContentScript function");
+	} else {
+		let vidDuration = vid.duration;
+		loopStopTime = loopStopTime < vidDuration ? loopStopTime : vidDuration;
+		vid.addEventListener("timeupdate", videoLoopHandler(loopStartTime, loopStopTime));
+	}
+}
 
-// 	debugMessage(`max speed: ${MAX_SPEED}`)
+function videoLoopHandler(loopStartTime, loopStopTime) {
+	debugMessage("the this object: ", this);
+	debugMessage(`loop start ${loopStartTime}, loop stop: ${loopStopTime}`);
 
-// 	if (this.currentTime >= loopStop || this.currentTime < loopStart) {
-// 		this.currentTime = loopStart;
-// 		debugMessage(`updated time to ${this.currentTime}`);
-// 	} else {
-// 		debugMessage("no time update!");
-// 	}
-// }
+	if (loopStopTime <= this.currentTime || this.currentTime < loopStartTime) {
+		this.currentTime = loopStartTime;
+		debugMessage(`updated time to ${this.currentTime}`);
+	} else {
+		debugMessage("no time update!");
+	}
+}
